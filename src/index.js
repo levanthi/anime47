c50254810410d3d38c6f469b7e872abba7ecf941
I Karos <aotamez1@gmail.com>
2022-05-26 12:47:40 +0700

    upadate

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import {BrowserRouter as Router} from 'react-router-dom'
 import App from './App';
 
 ReactDOM.render(
-  <Router>
+  <Router basename='/anime47'>
     <React.StrictMode>
       <App />
     </React.StrictMode>
